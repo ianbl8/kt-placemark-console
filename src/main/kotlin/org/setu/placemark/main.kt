950f8c21f3d48ad3832304b10da03423ee4552cc
Ian Blake <ian@ianbl8.com>
2023-09-10 17:13:55 +0100

    Adding initial menu support

--- src/main/kotlin/org/setu/placemark/main.kt
+++ src/main/kotlin/org/setu/placemark/main.kt
@@ -5,6 +5,42 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>){
-    println("Placemark Kotlin App Version 1.0")
     logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You chose Add Placemark")
+            2 -> println("You chose Update Placemark")
+            3 -> println("You chose List All Placemarks")
+            -1 -> println("Exiting Placemark App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting down Placemark Console App" }
+}
+
+fun menu(): Int {
+
+    var option: Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Placemark")
+    println(" 2. Update Placemark")
+    println(" 3. List All Placemarks")
+    println("-1. Exit")
+    println()
+    print("Enter an integer: ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+
+    return option
 }
