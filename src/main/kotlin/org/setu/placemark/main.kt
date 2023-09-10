31bb3e7e100c6ab427aedeb334e53c2f580dfa0e
Ian Blake <ian@ianbl8.com>
2023-09-10 17:03:23 +0100

    Adding logging support

--- src/main/kotlin/org/setu/placemark/main.kt
+++ src/main/kotlin/org/setu/placemark/main.kt
@@ -1,5 +1,10 @@
 package org.setu.placemark
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>){
     println("Placemark Kotlin App Version 1.0")
+    logger.info { "Launching Placemark Console App" }
 }
